6a9a9c579aa6e16de60eb3a129da249044bfa4ee
gelatoCode <sitimainura97@gmail.com>
2023-04-22 23:52:17 +0800

    updated-done

--- app.js
+++ app.js
@@ -29,6 +29,31 @@ app.get('/edit/:id' , (req, res) => {
         res.render('update' , {data : edit});
     })
 })
+
+app.post('/update/:id', (req, res) => {
+    fs.readFile('./free.json' , 'utf-8' , (err,data) => {
+
+        const read = JSON.parse(data);
+
+        const {name, char, swim} = req.body
+        const id = parseInt(req.params.id)
+
+        const edit = {
+            id, name, char, swim 
+        }
+
+        const update = read.findIndex((u) => u.id === edit.id)
+        read[update] = edit;
+
+        fs.writeFile('./free.json' , JSON.stringify(read) , err => {
+            if (err) throw err;
+        })
+
+        res.redirect('/');
+
+    })
+})
+
 app.listen(8080, () => {
     console.log('Processing ongoing .....')
 })
